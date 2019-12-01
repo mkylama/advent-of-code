cdb9f07367a22a802cc4f2c0c09ba3c4e02c4afd
Mikko K <mikko.kylama@gmail.com>
2019-12-01 19:44:06 +0200

    Add 2019 Day 1

---
+++ 2019/0101.py
@@ -0,0 +1,29 @@
+import math
+
+with open('input/01.txt', 'r') as file:
+    _input = file.read().strip().split('\n')
+
+def fuel1(f):
+	return math.floor(int(i) / 3) - 2
+
+total = 0
+
+for i in _input:
+	total = total + fuel1(int(i))
+
+print('0101: {}'.format(total))
+
+def fuel2(f):
+	fc = math.floor(f / 3) - 2
+	if fc < 0:
+		fc = 0
+	if fc > 0:
+		fc = fc + fuel2(fc)
+	return fc
+
+total = 0
+
+for i in _input:
+	total = total + fuel2(int(i))
+
+print('0102: {}'.format(total))
