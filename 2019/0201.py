67eb832242db77f1f68f72662620a48750cc029b
Mikko Kylämä <mikko.kylama@gmail.com>
2019-12-02 08:00:17 +0200

    Add Day 2 (2019)

---
+++ 2019/0201.py
@@ -0,0 +1,29 @@
+with open('input/02.txt', 'r') as file:
+    _input = file.read().strip().split(',')
+
+_input = list(map(int, _input))
+
+
+def gravity(i, a, b):
+	i = i.copy()
+	i[1] = a
+	i[2] = b
+
+	pos = 0
+
+	while i[pos] != 99:
+		if i[pos] == 1:
+			i[i[pos+3]] = i[i[pos+1]] + i[i[pos+2]]
+		elif i[pos] == 2:
+			i[i[pos+3]] = i[i[pos+1]] * i[i[pos+2]]
+		pos = pos + 4
+
+	return (i[0])
+
+print('0201: {}'.format(gravity(_input, 12, 2)))
+
+for noun in range(0, 99):
+	for verb in range(0, 99):
+		if gravity(_input, noun, verb) == 19690720:
+			print('0202: {}'.format(100 * noun + verb))
+			break
