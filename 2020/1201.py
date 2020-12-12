a09c5fa596a55f9d11398a696c7cc9a71bf73fa6
Mikko K <mikko.kylama@gmail.com>
2020-12-12 11:30:18 +0200

    2020/12 replace ifs with math and sorcery

--- 2020/1201.py
+++ 2020/1201.py
@@ -2,25 +2,41 @@ with open('input/12.txt', 'r') as file:
     _input = [x.strip() for x in file.readlines()]
 
 
-directions = ['N', 'E', 'S', 'W']
+directions = ['S', 'W', 'N', 'E']
 lr_values = {'R': 1, 'L': -1}
+
+
+def move_coord(coords, direction, value):
+    i = directions.index(direction)
+    coords[abs(i % 2 - 1)] += value * int((i // 2 - .5) * 2)
 
 
 def rotate_ship(current, left_or_right, angle):
     ci = directions.index(current)
-    ci = int((ci + angle / 90 * lr_values[left_or_right]) % 4)
+    ci = (ci + angle // 90 * lr_values[left_or_right]) % 4
     return directions[ci]
 
 
-def move_ship(location, direction, value):
+def rotate_waypoint(waypoint, left_or_right, angle): 
+    for i in range(0, angle // 90):
+        waypoint.reverse()
+        waypoint[bool(lr_values[left_or_right] + 1)] *= -1
+
+
+def move_waypoint(coords, direction, value):
     if direction == 'N':
-        location[1] += value
+        coords[1] += value
     elif direction == 'E':
-        location[0] += value
+        coords[0] += value
     elif direction == 'S':
-        location[1] -= value
+        coords[1] -= value
     elif direction == 'W':
-        location[0] -= value
+        coords[0] -= value
+
+
+def move_ship(location, waypoint, multiplier):
+    for i in [0, 1]:
+        location[i] += waypoint[i] * multiplier
 
 
 def navigate():
@@ -33,38 +49,13 @@ def navigate():
         v = int(inst[1:])
 
         if a in directions:
-            move_ship(location, a, v)
+            move_coord(location, a, v)
         elif a in lr_values:
             direction = rotate_ship(direction, a, v)
         elif a == 'F':
-            move_ship(location, direction, v)
+            move_coord(location, direction, v)
 
     return abs(location[0]) + abs(location[1])
-
-
-def rotate_waypoint(waypoint, left_or_right, angle): 
-    for i in range(0, int(angle / 90)):
-        waypoint.reverse()
-        if left_or_right == 'L':
-            waypoint[0] *= -1
-        else:
-            waypoint[1] *= -1
-
-
-def move_waypoint(location, direction, value):
-    if direction == 'N':
-        location[1] += value
-    elif direction == 'E':
-        location[0] += value
-    elif direction == 'S':
-        location[1] -= value
-    elif direction == 'W':
-        location[0] -= value
-
-
-def move_ship_toward_waypoint(location, waypoint, multiplier):
-    location[0] += waypoint[0] * multiplier
-    location[1] += waypoint[1] * multiplier
 
 
 def navigate_with_waypoint():
@@ -76,11 +67,11 @@ def navigate_with_waypoint():
         v = int(inst[1:])
 
         if a in directions:
-            move_waypoint(waypoint, a, v)
+            move_coord(waypoint, a, v)
         elif a in lr_values:
             direction = rotate_waypoint(waypoint, a, v)
         elif a == 'F':
-            move_ship_toward_waypoint(location, waypoint, v)
+            move_ship(location, waypoint, v)
 
     return abs(location[0]) + abs(location[1])
 
