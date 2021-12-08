acb64c0da0bff56c2f09f54d1a308caacc8f2f3d
Mikko K <mikko.kylama@gmail.com>
2021-12-08 18:04:00 +0200

    Cleanup

--- 2021/0801.py
+++ 2021/0801.py
@@ -3,38 +3,39 @@ with open('input/08.txt', 'r') as file:
 
 print('0801:', sum([sum([1 for o in l[1] if len(o) in [2, 3, 4, 7]]) for l in _input]))
 
-def _and(a, b):
-    return [x for x in a if x in b]
+def overlap(a, b):
+    return len([x for x in a if x in b])
 
 def get_output(line):
     digits = [''] * 10
-    # 1 7 4 8
+    
+    # 1 4 7 8
     for pattern in line[0]:
         if len(pattern) == 2:
             digits[1] = pattern
-        elif len(pattern) == 3:
-            digits[7] = pattern
         elif len(pattern) == 4:
             digits[4] = pattern
+        elif len(pattern) == 3:
+            digits[7] = pattern
         elif len(pattern) == 7:
             digits[8] = pattern
 
-    # 9 6 0 2 3 5
+    # 2 3 5 6 9 0
     for pattern in line[0]:
-        if len(pattern) == 6:
-            if len(_and(pattern, digits[4])) == 4:
-                digits[9] = pattern
-            elif len(_and(pattern, digits[7])) == 2:
-                digits[6] = pattern
-            else:
-                digits[0] = pattern
-        elif len(pattern) == 5:
-            if len(_and(pattern, digits[4])) == 2:
+        if len(pattern) == 5:
+            if overlap(pattern, digits[4]) == 2:
                 digits[2] = pattern
-            elif len(_and(pattern, digits[7])) == 3:
+            elif overlap(pattern, digits[1]) == 2:
                 digits[3] = pattern
             else:
                 digits[5] = pattern
+        elif len(pattern) == 6:
+            if overlap(pattern, digits[1]) == 1:
+                digits[6] = pattern
+            elif overlap(pattern, digits[4]) == 4:
+                digits[9] = pattern
+            else:
+                digits[0] = pattern
 
     return int(''.join([str(digits.index(x)) for x in line[1]]))
 
