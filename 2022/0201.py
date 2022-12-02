8d30483bc014d33869e0b666e9a12ca4dbca51c2
Mikko K <mikko.kylama@gmail.com>
2022-12-02 11:31:45 +0200

    Cleanup 2022 day 2

--- 2022/0201.py
+++ 2022/0201.py
@@ -1,5 +1,6 @@
 with open('input/02.txt', 'r') as file:
     _input = [[ord(x[0])-64, ord(x[2])-87] for x in file.readlines()]
 
-print('0201:', sum([round[1] + (round[1]-round[0]+4)%3*3 for round in _input]))
-print('0202:', sum([(round[0]+round[1])%3+1 + round[1]*3-3 for round in _input]))
+#                   shape score + outcome score
+print('0201:', sum([p2          + (p2-p1+1)%3*3 for (p1, p2) in _input]))
+print('0202:', sum([(p1+oc)%3+1 + oc*3-3        for (p1, oc) in _input]))
