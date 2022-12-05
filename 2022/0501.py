73074d8b6938ff0eb432257a40d788452750fc65
Mikko K <mikko.kylama@gmail.com>
2022-12-05 11:57:53 +0200

    Use strings instead of lists in 2022 day 5

--- 2022/0501.py
+++ 2022/0501.py
@@ -1,31 +1,23 @@
-def create_stacks(rows, size):
-    stacks = [[] for x in range(size)]
-
-    for containers in reversed(rows):
-        for idx, container in enumerate(containers):
-            if container != ' ':
-                stacks[idx].append(container)
-
-    return stacks
+with open('input/05.txt', 'r') as file:
+    _input1, _input2 = file.read().rstrip().split('\n\n')
 
 
 def rearrange(stacks, moves, reverse=False):
     for move in moves:
-        (c, f, t) = [int(x) for x in move.split(' ')[1::2]]
-        stacks[t-1].extend(stacks[f-1][-c:] if reverse else reversed(stacks[f-1][-c:]))
+        c, f, t = [int(x) for x in move.split(' ')[1::2]]
+        stacks[t-1] += stacks[f-1][-c:][::1 if reverse else -1]
         stacks[f-1] = stacks[f-1][:-c]
-
     return ''.join([x[-1] for x in stacks])
 
 
-with open('input/05.txt', 'r') as file:
-    _input1, _input2 = file.read().split('\n\n')
+rows = [x[1::4] for x in _input1.split('\n')][:-1]
+stacks = [''] * len(rows[0])
+for containers in reversed(rows):
+    for idx, container in enumerate(containers):
+        if container != ' ':
+            stacks[idx] += container
 
-rows = [x[1::4] for x in _input1.split('\n')]
-stack_count = len(rows.pop())
-moves = _input2.strip().split('\n')
+moves = _input2.split('\n')
 
-stacks = create_stacks(rows, stack_count)
-
-print('0501:', rearrange([x[:] for x in stacks], moves))
-print('0502:', rearrange([x[:] for x in stacks], moves, reverse=True))
+print('0501:', rearrange(stacks[:], moves))
+print('0502:', rearrange(stacks[:], moves, reverse=True))
