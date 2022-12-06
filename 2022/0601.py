a134fcbb4f2b550d47c09ee1b5f6b97106db6b65
Mikko K <mikko.kylama@gmail.com>
2022-12-06 08:24:42 +0200

    You know what

--- 2022/0601.py
+++ 2022/0601.py
@@ -1,10 +1,8 @@
 with open('input/06.txt', 'r') as file:
     _input = list(file.read().strip())
 
-def process(message, distinct):
-    for i in range(distinct,len(_input)):
-        if len(set(_input[i-distinct:i])) == distinct:
-            return i
+def process(msg, dist):
+    return [i for i in range(dist, len(msg)) if len(set(msg[i-dist:i])) == dist][0]
 
-print('0601', process(_input, 4))
-print('0602', process(_input, 14))
+print('0601:', process(_input, 4))
+print('0602:', process(_input, 14))
