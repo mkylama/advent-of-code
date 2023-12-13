c3ab3b3dc793a6a4e2a19806dbeda4bfc47e838b
mkylama <mikko.kylama@gmail.com>
2023-12-13 12:13:40 +0200

    Update 2023 day 13

--- 2023/1301.py
+++ 2023/1301.py
@@ -4,13 +4,13 @@ with open('input/13.txt', 'r') as file:
 def rotate(l):
     return [''.join([l[len(l)-y-1][x] for y in range(len(l))]) for x in range(len(l[0]))]
 
-def mirror(l, smudge=False):
+def mirror(l, smudge=0):
     for i in range(1, len(l)):
         linecount = min(i, len(l) - i)
         diffs = sum(1 for a, b in zip(''.join(l[i-linecount:i]), ''.join(reversed(l[i:i+linecount]))) if a != b)
-        if (smudge and diffs == 1) or (not smudge and diffs == 0):
+        if diffs == smudge:
             return i
     return 0
 
 print('1301:', sum([100 * mirror(p) + mirror(rotate(p)) for p in _input]))
-print('1302:', sum([100 * mirror(p, True) + mirror(rotate(p), True) for p in _input]))
+print('1302:', sum([100 * mirror(p, 1) + mirror(rotate(p), 1) for p in _input]))
