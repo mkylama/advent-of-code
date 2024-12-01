bba30143d5bb19d0ac4a26549e2d5f99f9946781
Mikko K <mikko.kylama@gmail.com>
2024-12-01 14:07:00 +0200

    Fix 2024 day 1

--- 2024/0101.py
+++ 2024/0101.py
@@ -1,4 +1,4 @@
-with open('input/01t.txt', 'r') as file:
+with open('input/01.txt', 'r') as file:
     _input = [[int(y) for y in x.split('   ')] for x in file.read().strip().split('\n')]
 
 left, right = zip(*_input)
