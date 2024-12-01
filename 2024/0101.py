015c7adea7a68a52b7d0ab9a299ee52487341ae7
Mikko K <mikko.kylama@gmail.com>
2024-12-01 15:33:13 +0200

    Refactor 2024 day 1

--- 2024/0101.py
+++ 2024/0101.py
@@ -1,7 +1,7 @@
 with open('input/01.txt', 'r') as file:
-    _input = [[int(y) for y in x.split('   ')] for x in file.read().strip().split('\n')]
+    _input = [[int(y) for y in x.split()] for x in file.read().strip().split('\n')]
 
 left, right = zip(*_input)
 
-print('0101:', sum([abs(x[0] - x[1]) for x in zip(sorted(left), sorted(right))]))
-print('0102:', sum([x * right.count(x) for x in left]))
+print('0101:', sum([abs(l - r) for l, r in zip(sorted(left), sorted(right))]))
+print('0102:', sum([l * right.count(l) for l in left]))
