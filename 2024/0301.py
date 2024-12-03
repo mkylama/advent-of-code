611fdece512528de500572594595228dc6fbd4dc
Mikko K <mikko.kylama@gmail.com>
2024-12-03 11:39:28 +0200

    Optimize 2024 day 3

--- 2024/0301.py
+++ 2024/0301.py
@@ -3,5 +3,5 @@ import re
 with open('input/03.txt', 'r') as file:
     _input = file.read().strip()
 
-print('0301:', sum([[int(a)*int(b) for (a, b) in re.findall(r'(\d+),(\d+)', x)][0] for x in re.findall(r'mul\(\d+,\d+\)', _input)]))
-print('0302:', sum([[int(a)*int(b) for (a, b) in re.findall(r'(\d+),(\d+)', x)][0] for x in re.findall(r'mul\(\d+,\d+\)', re.sub(r'(?s)don\'t\(\).*?(?=do\(\)|$)', '', _input))]))
+print('0301:', sum([int(a)*int(b) for (a, b) in re.findall(r'mul\((\d+),(\d+)\)', _input)]))
+print('0302:', sum([int(a)*int(b) for (a, b) in re.findall(r'mul\((\d+),(\d+)\)', re.sub(r'(?s)don\'t\(\).*?(?=do\(\)|$)', '', _input))]))
