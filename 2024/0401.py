e26ae9b23f23d7f2d51953be0f1dbdc53ea3883a
Mikko K <mikko.kylama@gmail.com>
2024-12-04 21:23:34 +0200

    Add 2024 day 4

---
+++ 2024/0401.py
@@ -0,0 +1,24 @@
+with open('input/04.txt', 'r') as file:
+    _input = file.read().strip().split('\n')
+
+cc = len(_input[0])
+rc = len(_input)
+
+def find_xmas(r, c):
+    found = 0
+    for d in [[0,1], [1,0], [0,-1], [-1,0], [1,1], [-1,1], [1,-1], [-1,-1]]:
+        if 0 <= r + 3 * d[0] < rc and 0 <= c + 3 * d[1] < cc:
+            if ''.join([_input[r+x*d[0]][c+x*d[1]] for x in range(4)]) == 'XMAS':
+                found += 1
+    return found
+
+def find_x_mas(r, c):
+    if 0 < r < rc - 1 and 0 < c < cc - 1:
+        se = ''.join([_input[r+x[0]][c+x[1]] for x in [[-1,-1], [0,0], [1,1]]])
+        sw = ''.join([_input[r+x[0]][c+x[1]] for x in [[-1,1], [0,0], [1,-1]]])
+        if se in ['MAS', 'SAM'] and sw in ['MAS', 'SAM']:
+            return 1
+    return 0
+
+print('0401:', sum([sum([find_xmas(x, y) for x in range(cc) if _input[x][y] == 'X']) for y in range(rc)]))
+print('0402:', sum([sum([find_x_mas(x, y) for x in range(cc) if _input[x][y] == 'A']) for y in range(rc)]))
